157dfa410a6a3ca69acc3872e5f878d543c732a4
deivid.fabiano <deivid.fabiano@bv.com.br>
2022-04-20 17:50:55 -0300

    Add SQL Exemplos

--- sql_exemplos.sql
+++ sql_exemplos.sql
@@ -19,3 +19,47 @@ UPDATE usuarios set nome = 'Deivid Willyan', password = 'ABC123' where id = 1;
 
 # APAGA na tabela de usuarios onde o id seja igual a 2
 DELETE from usuarios where id = 2;
+
+# busca todas as informações da tabela noticias
+SELECT * FROM noticias;
+
+# insete na tabela noticas informando os campos titulo, descricao e id_usuarios os valores
+insert into noticias (titulo, descricao, id_usuario) values ('Noticia XPTO', 'Se Chove a Terra Molha!!!!', 1);
+insert into noticias (titulo, descricao, id_usuario) values ('Noticia Quente', 'Hoje fez 30 graus', 2);
+
+# atualiza na tabela noticia o titlu onde a id da noticia for 1 e o id do usuario foi 1.
+update noticias set titulo = 'Noticia da Chuva' where id = 1 and id_usuario = 1;
+
+# busca todas as noticias para o usuario com id 1
+select * from noticias where id_usuario = 1;
+
+# titulo noticias, descricao noticia, dt atualizacao noticia, nome do usuario que incluiu a noticia
+SELECT
+	n.titulo as 'Titulo',
+    n.descricao as 'Descricão',
+    n.dt_autalizacao as 'Ultima Atualizacão',
+    u.nome as 'Autor'
+FROM noticias n
+INNER JOIN usuarios u on u.id = n.id_usuario
+WHERE u.nome like 'D%';
+
+# realiza a busca juntando as tabelas de noticias e usuario pelo id e ordenando pela dt atualizacao decrescente.
+SELECT
+	n.titulo as 'Titulo',
+    n.descricao as 'Descricão',
+    n.dt_autalizacao as 'Ultima Atualizacão',
+    u.nome as 'Autor'
+FROM noticias n
+INNER JOIN usuarios u on u.id = n.id_usuario
+ORDER BY n.dt_autalizacao DESC;
+
+# busca todas as noticias limitando o resultado para 2 items.
+SELECT * FROM noticias limit 2;
+
+
+
+
+
+
+
+
