310d11df371b1b5a6f517acd720670a589e5917c
deivid.fabiano <deivid.fabiano@bv.com.br>
2022-04-21 15:42:43 -0300

    MySql Configuration

--- bin/backend.dart
+++ bin/backend.dart
@@ -6,9 +6,23 @@ import 'infra/custom_server.dart';
 import 'infra/dependency_injector/injects.dart';
 import 'infra/middleware_interception.dart';
 import 'utils/custom_env.dart';
+import 'package:mysql1/mysql1.dart';
 
 void main() async {
   CustomEnv.fromFile('.env-dev');
+
+  var conexao = await MySqlConnection.connect(
+    ConnectionSettings(
+      host: await CustomEnv.get<String>(key: 'db_host'),
+      port: await CustomEnv.get<int>(key: 'db_port'),
+      user: await CustomEnv.get<String>(key: 'db_user'),
+      password: await CustomEnv.get<String>(key: 'db_pass'),
+      db: await CustomEnv.get<String>(key: 'db_schema'),
+    ),
+  );
+
+  var result = await conexao.query('SELECT * FROM usuarios;');
+  print(result);
 
   final _di = Injects.initialize();
 
